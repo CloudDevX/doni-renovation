10273e7a3cd06dc87b8c17b4b2fe524ed6a87d26
CloudDevX <75575523+CloudDevX@users.noreply.github.com>
2021-02-09 13:33:29 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -2,30 +2,14 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Contact</title>
+    <title>Envoi d'un message par formulaire</title>
 </head>
 
 <body>
-    <h1>Contact</h1>
-    <form method="post">
-        <label>Email</label>
-        <input type="email" name="email" required><br>
-        <label>Message</label>
-        <textarea name="message" required></textarea><br>
-        <input type="submit">
-    </form>
     <?php
-    if (isset($_POST['message'])) {
-        $position_arobase = strpos($_POST['email'], '@');
-        if ($position_arobase === false)
-            echo '<p>Votre email doit comporter un arobase.</p>';
-        else {
-            $retour = mail('UNMAIL@ICI.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From: ' . $_POST['email']);
-            if($retour)
-                echo '<p>Votre message a été envoyé.</p>';
-            else
-                echo '<p>Erreur.</p>';
-        }
+    $retour = mail('jules@free.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From : webmaster@monsite.fr');
+    if ($retour) {
+        echo '<p>Votre message a bien été envoyé.</p>';
     }
     ?>
 </body>
